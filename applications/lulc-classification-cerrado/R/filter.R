3b1ac218e56685269efc1adf9d9e78f87f5eca28
Phelps <phelpsmemo@gmail.com>
2021-12-23 03:40:49 -0300

    add datacube classification function

--- applications/lulc-classification-cerrado/R/filter.R
+++ applications/lulc-classification-cerrado/R/filter.R
@@ -1,4 +1,5 @@
 
+
 #' @title Annual sample filter
 #'
 #' @rdname filter
@@ -13,10 +14,12 @@
 #' @return todo
 #' @export
 filter_annual_samples <- function(samples_ts) {
-  anual_samples <- c();
+  anual_samples <- c()
+  
   for (i in 1:length(samples_ts$time_series)) {
-    anual_samples <- c(anual_samples, nrow(samples_ts$time_series[[i]]) == 24)
+    anual_samples <-
+      c(anual_samples, nrow(samples_ts$time_series[[i]]) == 24)
   }
- 
-  samples_ts[anual_samples, ]
+  
+  samples_ts[anual_samples,]
 }
