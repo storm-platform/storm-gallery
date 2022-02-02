98eef57fb80ac214d3caf1446aa0c01d8541ad74
Felipe Carlos <efelipecarlos@gmail.com>
2022-02-02 13:48:59 +0000

    add build script for bdc-article package

---
+++ applications/bdc-article/compendium/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 Storm Gallery.
+#
+# storm-gallery is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+docker build --no-cache -t "stormproject/pkg-bdc-article:0.1" .
+
