4b8faf985a6d02129228f1b2884ae0933274fe4f
Felipe Carlos <efelipecarlos@gmail.com>
2022-02-02 19:17:42 +0000

    fix run script

--- applications/deforestation-detection/compendium/endmember-selection/run.sh
+++ applications/deforestation-detection/compendium/endmember-selection/run.sh
@@ -6,9 +6,9 @@
 # storm-gallery is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-docker run --rm -ti \
+docker run -d -ti \
 	--name pkg-endmember-selection \
-	--publish 8889:8888 \
-	--volume ${PWD}:/home/${NB_USER}/endmember-selection \
+	--publish 8888:8888 \
+	--volume ${PWD}/data/derived_data/:/home/jovyan/endmember-selection/data/derived_data \
 	stormproject/pkg-endmember-selection:0.1
 
