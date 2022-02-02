a94344894943694363988be7cc4ef319a7d40709
Felipe Carlos <efelipecarlos@gmail.com>
2022-02-02 13:53:33 +0000

    madd run  script

---
+++ applications/bdc-article/compendium/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 Storm Gallery.
+#
+# storm-gallery is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+docker run --rm -ti \
+	--name pkg-bdc-article \
+	--publish 8888:8888 \
+	--volume ${PWD}:/home/${NB_USER}/pkg-bdc-article \
+	stormproject/pkg-bdc-article:0.1
+
