aff893a8cd129000875cdb5c09b634c61ab7a20e
Felipe Carlos <efelipecarlos@gmail.com>
2022-02-02 13:03:11 +0000

    cleaning invalid data

---
+++ applications/deforestation-detection/compendium/endmember-selection/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 Storm Gallery.
+#
+# storm-gallery is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+docker run --rm -ti \
+	--name pkg-endmember-selection \
+	--publish 8888:8888 \
+	--volume ${PWD}:/home/jovyan/endmember-selection \
+	stormproject/pkg-endmember-selection:0.1
+
