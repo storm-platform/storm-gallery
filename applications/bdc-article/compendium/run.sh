90bb307fe5c13e0de63ffcd43ecb9f7097fcb3a5
Felipe Carlos <efelipecarlos@gmail.com>
2022-02-03 16:45:43 +0000

    update run script to background container

--- applications/bdc-article/compendium/run.sh
+++ applications/bdc-article/compendium/run.sh
@@ -6,7 +6,7 @@
 # storm-gallery is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-docker run --rm -ti \
+docker run -d -ti \
 	--name pkg-bdc-article \
 	--publish 8888:8888 \
 	--volume ${PWD}:/home/${NB_USER}/pkg-bdc-article \
