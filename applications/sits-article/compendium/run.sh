1d3493f639c2809562f882fd8176bb492702675a
Felipe Carlos <efelipecarlos@gmail.com>
2022-02-11 04:10:16 +0000

    update run script

--- applications/sits-article/compendium/run.sh
+++ applications/sits-article/compendium/run.sh
@@ -6,9 +6,9 @@
 # storm-gallery is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-docker run --rm -ti \
+docker run -d -ti \
 	--name pkg-sits-article \
 	--publish 8888:8888 \
-	--volume ${PWD}:/home/${NB_USER}/pkg-sits-article \
+	--volume ${PWD}/data:/home/jovyan/sits-article/data \
 	stormproject/pkg-sits-article:0.1
 
