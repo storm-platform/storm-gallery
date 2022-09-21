cc5da418a4b7f118aa7be046b7578a8474a75877
Felipe Carlos <efelipecarlos@gmail.com>
2022-09-21 05:30:59 -0300

    deforestation: adding reproduction workflow and preparing environment to create the reproducibility analysis

---
+++ applications/deforestation-detection/workflow/05-rep-endmember-selection.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 Storm Gallery.
+#
+# storm-gallery is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+#
+# General definitions
+#
+
+# Experiment step path
+STEP_PATH=compendium/endmember-selection
+
+STEP_EXECUTION_COMPENDIUM_STEP=data/derived_data/execution-compendium
+
+EXECUTION_COMPENDIUM_FILE=endmember-selection.zip
+
+#
+# 1. Accessing the compendium step directory
+#
+cd ${STEP_PATH}
+
+#
+# 2. Accessing the execution compendium directory
+#
+cd ${STEP_EXECUTION_COMPENDIUM_STEP}
+
+#
+# 3. Extracting the execution compendium data
+#
+workbench import compendium -f ${EXECUTION_COMPENDIUM_FILE} -o files
+
+#
+# 4. Reproducing
+#
+cd files
+
+workbench exec rerun
