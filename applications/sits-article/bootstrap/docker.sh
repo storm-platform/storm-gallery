d5fd96fe5d90854d82796961d1f9121d8ee2ef70
Felipe Carlos <efelipecarlos@gmail.com>
2022-11-13 06:40:05 -0300

    application: updating sits article

--- applications/sits-article/bootstrap/docker.sh
+++ applications/sits-article/bootstrap/docker.sh
@@ -6,23 +6,12 @@
 # under the terms of the MIT License; see LICENSE file for more details.
 #
 
-#
-# 1. Updating the environment
-#
-apt-get update -y
-apt-get install -y apt-get install \
-            ca-certificates \
-            curl \
-            gnupg \
-            lsb-release \
-            vim \
-            htop
 
 #
-# 2. Installing Docker
+# 1. Installing Docker
 #
 
-# 2.1. Download
+# 1.1. Download
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
   | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
@@ -33,21 +22,21 @@ echo \
 apt-get update -y
 apt-get install docker-ce docker-ce-cli containerd.io -y
 
-# 2.2. Configure the user to use Docker
+# 1.2. Configure the user to use Docker
 groupadd docker
 usermod -aG docker vagrant
 
 #
-# 3. Installing Docker Compose
+# 2. Installing Docker Compose
 #
 
-# 3.1. Download
+# 2.1. Download
 curl -L \
   "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
   -o /usr/local/bin/docker-compose
 
-# 3.2. Make it executable
+# 2.2. Make it executable
 chmod +x /usr/local/bin/docker-compose
 
-# 3.3. Linking it
+# 2.3. Linking it
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
---
+++ applications/sits-article/workflow/matogrosso/02-rep-prepare-environment.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 Storm Gallery.
+#
+# storm-gallery is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+#
+# General definitions
+#
+PYTHON_VERSION=3.8.0
+
+PYTHON_VIRTUALENV=matogrosso-classification
+
+#
+# 1. Installing python
+#
+pyenv install ${PYTHON_VERSION}
+
+#
+# 2. Configuring the reproduction environment using pyenv
+#
+pyenv virtualenv ${PYTHON_VERSION} ${PYTHON_VIRTUALENV}
+pyenv local ${PYTHON_VIRTUALENV}
+
+pip install --upgrade pip
+
+#
+# 3. Installing the dependencies
+#
+
+# 3.1. Installing poetry
+pip install poetry
+poetry config virtualenvs.create false
+
+# 3.2. Installing reproduction dependency
+poetry install
